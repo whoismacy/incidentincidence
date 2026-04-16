48f096b61c398821e4e0907f792f9db566d440db
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2026-04-16 13:06:43 +0300

    Add gitattribute & SegmentedButtons

--- app/src/main/java/com/whoismacy/android/incidentincidence/view/SortFilterComponent.kt
+++ app/src/main/java/com/whoismacy/android/incidentincidence/view/SortFilterComponent.kt
@@ -1,19 +1,17 @@
 package com.whoismacy.android.incidentincidence.view
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -21,41 +19,52 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-private val sortValues =
-    listOf(
-        "Newest first",
-        "Oldest first",
-        "Shuffle",
-    )
+interface SortFilter {
+    val value: String
+}
 
-private val filterSevereValues =
-    listOf(
-        "Low",
-        "Medium",
-        "High",
-        "Severe",
-    )
+enum class SortValues(
+    override val value: String,
+) : SortFilter {
+    NEWEST("Newest first"),
+    OLDEST("Oldest first"),
+    SHUFFLE("Shuffle"),
+}
 
-private val filterPeriodValues =
-    listOf(
-        "Today",
-        "Past week",
-        "Past month",
-        "Past year",
-    )
+enum class FilterSevereValues(
+    override val value: String,
+) : SortFilter {
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High"),
+    SEVERE("Severe"),
+}
+
+enum class FilterPeriodValues(
+    override val value: String,
+) : SortFilter {
+    TODAY("Today"),
+    PWEEK("Past Week"),
+    PMONTH("Past Month"),
+    PYEAR("Past Year"),
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SortFilterComponent() {
-    var sortValues by remember { mutableStateOf(null) }
-    var filterSevereValues by remember { mutableStateOf(null) }
-    var filterPeriodValues by remember { mutableStateOf(null) }
+    var currentSortValue by remember { mutableStateOf(SortValues.NEWEST) }
+    var currentFilterSevereValue by remember { mutableStateOf(FilterSevereValues.LOW) }
+    var currentFilterPeriodValue by remember { mutableStateOf(FilterPeriodValues.TODAY) }
+
+    val changeSortValue: (SortValues) -> Unit = { value -> currentSortValue = value }
+    val changeFilterSevereValue: (FilterSevereValues) -> Unit = { value -> currentFilterSevereValue = value }
+    val changeFilterPeriodValue: (FilterPeriodValues) -> Unit = { value -> currentFilterPeriodValue = value }
+
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
     ModalBottomSheet(
@@ -88,23 +97,11 @@ fun SortFilterComponent() {
                 modifier = Modifier.fillMaxWidth(),
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                modifier =
-                    Modifier
-                        .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround,
-            ) {
-                Button(onClick = {}) {
-                    Text("Shuffle")
-                }
-                Button(onClick = {}) {
-                    Text("Newest First")
-                }
-                Button(onClick = {}) {
-                    Text("Oldest First")
-                }
-            }
+            SortFilterButtonGroup(
+                items = SortValues.entries.toList(),
+                selectedItem = currentSortValue,
+                changeSelectedItem = changeSortValue,
+            )
             Spacer(modifier = Modifier.height(32.dp))
             Text(
                 "Filter",
@@ -114,50 +111,19 @@ fun SortFilterComponent() {
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text("by Severity", style = MaterialTheme.typography.bodyMedium)
-            Row(
-                modifier =
-                    Modifier
-                        .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround,
-            ) {
-                Button(onClick = {}) {
-                    Text("Low")
-                }
-                Button(onClick = {}) {
-                    Text("Medium")
-                }
-                Button(onClick = {}) {
-                    Text("High")
-                }
-                Button(onClick = {}) {
-                    Text("Severe")
-                }
-            }
+            SortFilterButtonGroup(
+                items = FilterSevereValues.entries.toList(),
+                selectedItem = currentFilterSevereValue,
+                changeSelectedItem = changeFilterSevereValue,
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text("by Period", style = MaterialTheme.typography.bodyMedium)
-            Row(
-                modifier =
-                    Modifier
-                        .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Button(onClick = {}) {
-                    Text("Today")
-                }
-                Button(onClick = {}) {
-                    Text("Past Week")
-                }
-                Button(onClick = {}) {
-                    Text("Past Month")
-                }
-                Button(onClick = {}) {
-                    Text("Past Year")
-                }
-            }
-
+            SortFilterButtonGroup(
+                items = FilterPeriodValues.entries.toList(),
+                selectedItem = currentFilterPeriodValue,
+                changeSelectedItem = changeFilterPeriodValue,
+            )
             Spacer(modifier = Modifier.height(48.dp))
-
             OutlinedButton(
                 onClick = {},
                 modifier = Modifier.fillMaxWidth(),
@@ -169,14 +135,26 @@ fun SortFilterComponent() {
 }
 
 @Composable
-fun ConnectedButtonGroup(
-    textList: List<String>,
-    currentIndex: Int,
-    changecurrentIndex: (Int) -> Unit,
+fun <T : SortFilter> SortFilterButtonGroup(
+    items: List<T>,
+    selectedItem: T,
+    changeSelectedItem: (T) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-    ) { }
+    SingleChoiceSegmentedButtonRow(modifier = modifier) {
+        items.forEachIndexed { index, item ->
+            SegmentedButton(
+                selected = item == selectedItem,
+                onClick = { changeSelectedItem(item) },
+                label = { Text(item.value) },
+                shape =
+                    SegmentedButtonDefaults.itemShape(
+                        index = index,
+                        count = items.size,
+                    ),
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
