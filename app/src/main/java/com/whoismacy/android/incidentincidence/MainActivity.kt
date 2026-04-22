cc5d1fbcddb936de360074b2caab217c75c9c0e4
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2026-04-22 10:34:24 +0300

    Refactored MainScreen to use NavHost

--- app/src/main/java/com/whoismacy/android/incidentincidence/MainActivity.kt
+++ app/src/main/java/com/whoismacy/android/incidentincidence/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.whoismacy.android.incidentincidence.ui.theme.IncidentIncidenceTheme
+import com.whoismacy.android.incidentincidence.view.Fab
 import com.whoismacy.android.incidentincidence.view.MainScreen
 import com.whoismacy.android.incidentincidence.viewmodel.IncidentViewModel
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/whoismacy/android/incidentincidence/view/MainScreen.kt
+++ app/src/main/java/com/whoismacy/android/incidentincidence/view/MainScreen.kt
@@ -1,18 +1,35 @@
 package com.whoismacy.android.incidentincidence.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.whoismacy.android.incidentincidence.R
 import com.whoismacy.android.incidentincidence.viewScreens.HomeScreen
 import com.whoismacy.android.incidentincidence.viewScreens.SolvedIncidentsScreen
 import com.whoismacy.android.incidentincidence.viewmodel.IncidentViewModel
@@ -25,11 +42,13 @@ object Home
 @Serializable
 object SolvedIncidents
 
+@Serializable
+object NewIncident
+
 @Composable
 fun MainScreen(
     viewModel: IncidentViewModel = hiltViewModel(),
 ) {
-//    var destinations by rememberSaveable { mutableStateOf(Destinations.HOME) }
     val displayData by viewModel
         .displayIncidences
         .collectAsStateWithLifecycle(emptyList())
@@ -37,17 +56,91 @@ fun MainScreen(
     val navController = rememberNavController()
     val snackBarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+    var currentDestination: String by remember { mutableStateOf("Home") }
 
-    NavHost(
-        navController = navController,
-        startDestination = Home,
-    ) {
-        composable<Home> {
-            HomeScreen(incidences = displayData)
+    LaunchedEffect(Unit) {
+        navController.currentBackStackEntryFlow.collect { backStackEntry ->
+            currentDestination = backStackEntry
+                .destination.route
+                ?.substringBefore("?") ?: ""
         }
+    }
 
-        composable<SolvedIncidents> {
-            SolvedIncidentsScreen(incidences = displayData)
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = currentDestination.contains("Home"),
+                    onClick = {
+                        navController.navigate(Home) {
+                            popUpTo(Home) {
+                                inclusive = true
+                            }
+                        }
+                        currentDestination = "Home"
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_free_breakfast_24),
+                            contentDescription = null,
+                        )
+                    },
+                    label = {
+                        Text("Home")
+                    },
+                )
+
+                NavigationBarItem(
+                    selected = currentDestination.contains("SolvedIncidents"),
+                    onClick = {
+                        navController.navigate(SolvedIncidents) {
+                            popUpTo(SolvedIncidents) {
+                                inclusive = true
+                            }
+                        }
+                        currentDestination = "SolvedIncidents"
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_star_shine_24),
+                            contentDescription = null,
+                        )
+                    },
+                    label = {
+                        Text("Solved")
+                    },
+                )
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(snackBarHostState) { snackbarData ->
+                Snackbar(
+                    modifier =
+                        Modifier
+                            .padding(16.dp)
+                            .clip(RoundedCornerShape(12.dp)),
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    actionColor = MaterialTheme.colorScheme.primary,
+                    actionContentColor = MaterialTheme.colorScheme.onPrimary,
+                    shape = RoundedCornerShape(12.dp),
+                    snackbarData = snackbarData,
+                )
+            }
+        },
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Home,
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            composable<Home> {
+                HomeScreen(incidences = displayData)
+            }
+
+            composable<SolvedIncidents> {
+                SolvedIncidentsScreen(incidences = displayData)
+            }
         }
     }
 
@@ -67,63 +160,4 @@ fun MainScreen(
             }
         }
     }
-
-//    NavigationSuiteScaffold(
-//        navigationSuiteItems = {
-//            Destinations.entries.forEach {
-//                item(
-//                    selected = it == destinations,
-//                    onClick = { destinations = it },
-//                    icon = {
-//                        Icon(
-//                            painter = painterResource(it.icon),
-//                            contentDescription = it.contentDescription,
-//                        )
-//                    },
-//                    label = { Text(it.label) },
-//                )
-//            }
-//        },
-//        content = {
-//            Scaffold(
-//                topBar = {
-//                    SearchBar(displayFilterState.searchQuery)
-//                },
-//                modifier = Modifier.fillMaxSize().padding(top = 32.dp),
-//                floatingActionButton = { Fab() },
-//                snackbarHost = {
-//                    SnackbarHost(snackBarHostState) { snackbarData ->
-//                        Snackbar(
-//                            modifier =
-//                                Modifier
-//                                    .padding(16.dp)
-//                                    .clip(RoundedCornerShape(12.dp)),
-//                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-//                            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-//                            actionColor = MaterialTheme.colorScheme.primary,
-//                            actionContentColor = MaterialTheme.colorScheme.onPrimary,
-//                            shape = RoundedCornerShape(12.dp),
-//                            snackbarData = snackbarData,
-//                        )
-//                    }
-//                },
-//            ) { innerPadding ->
-//                when (destinations) {
-//                    Destinations.HOME -> {
-//                        Home(
-//                            incidences = displayData,
-//                            modifier = Modifier.padding(innerPadding),
-//                        )
-//                    }
-//
-//                    Destinations.SOLVED -> {
-//                        Solved(
-//                            incidences = displayData,
-//                            modifier = Modifier.padding(innerPadding),
-//                        )
-//                    }
-//                }
-//            }
-//        },
-//    )
 }
